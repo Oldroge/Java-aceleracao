2d7dd7eac5a9eddda0605c83c07afc3743439659
oldroge <rogeriomrjr@gmail.com>
2022-12-08 14:52:36 -0300

    feat: entendendo tipo primitivo short e formatação com uderscore

--- src/main/java/variables/types/PrimitiveTypes.java
+++ src/main/java/variables/types/PrimitiveTypes.java
@@ -6,6 +6,12 @@ public class PrimitiveTypes {
 //		BYTE
         byte b = 9;
         System.out.println(b);
+        
+//      SHORT
+        short num = 300;
+        short formatadaComUnderscore = 20_000;
+        System.out.println(num);
+        System.out.println(formatadaComUnderscore);
 	}
 
 }
