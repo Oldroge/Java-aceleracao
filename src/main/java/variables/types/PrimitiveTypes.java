c66eced736f272dd463200f618b88da261d8e46c
oldroge <rogeriomrjr@gmail.com>
2022-12-08 14:57:02 -0300

    feat: entendendo tipo primitivo int e inicialização de variável inline

--- src/main/java/variables/types/PrimitiveTypes.java
+++ src/main/java/variables/types/PrimitiveTypes.java
@@ -12,6 +12,13 @@ public class PrimitiveTypes {
         short formatadaComUnderscore = 20_000;
         System.out.println(num);
         System.out.println(formatadaComUnderscore);
+        
+//      INT
+        int numeroInteiro = 10;
+        int a = 10, c = 20, soma; // inicialização de variáveis inline
+        soma = a + c; // atribuição da soma da variável a + b para a variável soma
+        System.out.println(numeroInteiro);
+        System.out.println(soma);
 	}
 
 }
