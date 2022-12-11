045dfe842ed3aed9de74fb64c804b0d3055c8313
oldroge <rogeriomrjr@gmail.com>
2022-12-11 19:59:24 -0300

    feat: adicionando exemplos de matrizes com arrays

--- src/main/java/variables/types/PrimitiveTypes.java
+++ src/main/java/variables/types/PrimitiveTypes.java
@@ -101,6 +101,24 @@ public class PrimitiveTypes {
         System.out.println(arrayDeInteiros[2]); // 63
 
         arrayDeInteiros[9] = 89; // Estoura a pilha porque tentamos inserir um valor em um espaço inexistente do array
+        
+//      MATRIZES COM ARRAYS
+        System.out.println("MATRIZES COM ARRAYS");
+        int[] arrayDeInteiros1 = {1,2,3,4,5,6,767,878,8,8,9,9,9};
+        int[][] matriz = new int[2][10]; // inicializamos uma matriz, bem parecido com uma tabela de excel com coluna e linha
+        String[] arrayDeString = new String[10];
+
+        matriz[0][0] = 10;
+        matriz[1][0] = 20;
+
+        arrayDeString[0] = "Na Trybe";
+        arrayDeString[1] = " eu aprendo Java!";
+
+        System.out.println(arrayDeInteiros1[3]); // 4
+        System.out.println(matriz[0][0]); // 10
+        System.out.println(matriz[1][5]); // 0
+        System.out.println(arrayDeString[0] + arrayDeString[1]); // Na Trybe eu aprendo Java!
+        System.out.println(arrayDeString.length); // 10
 	}
 
 }
