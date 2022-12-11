b80f8126957a628bdbf9bf4f8adc1c41398cbab9
oldroge <rogeriomrjr@gmail.com>
2022-12-11 19:49:12 -0300

    feat: adicionando exemplos do tipo array

--- src/main/java/variables/types/PrimitiveTypes.java
+++ src/main/java/variables/types/PrimitiveTypes.java
@@ -84,6 +84,23 @@ public class PrimitiveTypes {
                 .append(java1)
                 .toString();
         System.out.println(frase2); // Eu já sei usar Strings em Java
+        
+//      ARRAYS
+        int[] arrayDeInteiros = new int[9];
+        arrayDeInteiros[0] = 40;
+        arrayDeInteiros[1] = 55;
+        arrayDeInteiros[2] = 63;
+        arrayDeInteiros[3] = 17;
+        arrayDeInteiros[4] = 22;
+        arrayDeInteiros[5] = 68;
+        arrayDeInteiros[6] = 89;
+        arrayDeInteiros[7] = 97;
+        arrayDeInteiros[8] = 89;
+
+        System.out.println(arrayDeInteiros[8]); // 89
+        System.out.println(arrayDeInteiros[2]); // 63
+
+        arrayDeInteiros[9] = 89; // Estoura a pilha porque tentamos inserir um valor em um espaço inexistente do array
 	}
 
 }
