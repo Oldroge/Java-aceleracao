3387c50ee1ca336bdf95118e0b510929899a8270
oldroge <rogeriomrjr@gmail.com>
2022-12-12 15:34:58 -0300

    feat: Adicionando estrutura adicional ao if/else com else if

--- src/main/java/structures/conditionals/IfElse.java
+++ src/main/java/structures/conditionals/IfElse.java
@@ -1,5 +1,17 @@
 package structures.conditionals;
 
 public class IfElse {
-
+	public static void main(String[] args) {
+		double mediaFinal = 7.5;
+		String resultado = "Indefinido";
+		
+		if (mediaFinal >= 7) {
+			resultado = "Aprovada";
+		} else if (mediaFinal == 6.0) {
+			resultado = "Realizar prova bonus";
+		}
+		else {
+			resultado = "Reprovada";
+		}
+	}
 }
