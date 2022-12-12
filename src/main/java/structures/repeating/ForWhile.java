15ce020d3494cb284477e69e43b8f90a4226b80f
oldroge <rogeriomrjr@gmail.com>
2022-12-12 16:08:54 -0300

    feat: adicionando laço for com usando com arrays

--- src/main/java/structures/repeating/ForWhile.java
+++ src/main/java/structures/repeating/ForWhile.java
@@ -43,5 +43,26 @@ public class ForWhile {
 			log2 = "O Número atual é: " + numero + "|n";
 			System.out.println(log2);
 		}
+		
+//	FOR COM ARRAYS
+	// Cria um vetor com 4 alunos
+		String[] alunos = { "MARCOS", "ALINE", "LUCAS", "PATRICIA" };
+		String log3;
+
+		// Pega o total de alunos existentes no vetor
+		int totalAlunos = alunos.length;
+
+		// Nossa variável 'log' não é muito útil agora, mas serve para registrarmos o
+		// que está acontecendo! ✌️
+		log3 = "A lista de chamadas possui " + totalAlunos + " alunos|n";
+
+		// Imprime a lista de chamadas. Note que você pode declarar a variável posição
+		// dentro do for!
+		int matricula;
+		for (int posicao = 0; posicao < totalAlunos; posicao++) {
+			matricula = posicao + 1;
+			log3 = "Matricula: " + matricula + " Aluno: " + alunos[posicao] + "|n";
+			System.out.println(log3);
+		}
 	}
 }
