7c834af4464d3540c595d2e97f38225e3ce66505
oldroge <rogeriomrjr@gmail.com>
2022-12-20 18:21:00 -0300

    feat: exemplo de como converter string em short usando classe wrapper

--- src/main/java/UsandoScanner.java
+++ src/main/java/UsandoScanner.java
@@ -13,13 +13,23 @@ class UsandoScanner {
         String input1 = scanner.next();
         System.out.print("Entre com o segundo número: ");
         String input2 = scanner.next();
-        scanner.close();
 
 	    	int n1 = Integer.parseInt(input1);
 		    int n2 = Integer.parseInt(input2);
 
         int resultado = n1 + n2;
         System.out.println("O resultado da soma é: " + resultado);
+        
+        System.out.print("Entre com a sua idade: ");
+        String input3 = scanner.next();
+        scanner.close();
+
+        short idade = Short.parseShort(input3);
+
+        short resultado2 = idade;
+        resultado2++;
+
+        System.out.println("Daqui a um ano sua idade será: " + resultado2);
     
     }
 
