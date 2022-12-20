adb9299766d8bc51a5d8a9c63f87220fb876634d
oldroge <rogeriomrjr@gmail.com>
2022-12-20 18:48:34 -0300

    feat: exemplo de conversão de string para long com parseLong

--- src/main/java/UsandoScanner.java
+++ src/main/java/UsandoScanner.java
@@ -22,7 +22,6 @@ class UsandoScanner {
         
         System.out.print("Entre com a sua idade: ");
         String input3 = scanner.next();
-        scanner.close();
 
         short idade = Short.parseShort(input3);
 
@@ -30,6 +29,16 @@ class UsandoScanner {
         resultado2++;
 
         System.out.println("Daqui a um ano sua idade será: " + resultado2);
+        
+        System.out.print("Entre com o tamanho da população chinesa: ");
+        String input4 = scanner.next();
+        scanner.close();
+    
+        long populacaoChinesa = Long.parseLong(input4);
+
+        // Obs: não se preocupe, logo mais descobriremos o porquê desse L aparecer no número `7909863848L`.
+        long resultado3 = 7909863848L + populacaoChinesa;
+        System.out.println("Se tivéssemos duas Chinas no mundo, a população mundial seria: " + resultado3);
     
     }
 
