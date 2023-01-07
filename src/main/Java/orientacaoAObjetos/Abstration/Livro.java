8621d2180f63e820a12ba1296ec1cb03dbdea871
oldroge <rogeriomrjr@gmail.com>
2023-01-06 22:44:52 -0300

    study: entendendo métodos que compoẽ ações da classe

--- src/main/Java/orientacaoAObjetos/Abstration/Livro.java
+++ src/main/Java/orientacaoAObjetos/Abstration/Livro.java
@@ -1,14 +1,19 @@
 package orientacaoAObjetos.Abstration;
 
 public class Livro {
+  String titulo;
+  int numeroDePaginas;
+  String isbn;
+
   public Livro(String message) {
-    String titulo;
-    int numeroDePaginas;
-    String isbn;
     System.out.println(message);
   }
 
   public Livro(int num) {
     System.out.println(num);
+  }
+
+  public int retornarNumeroDePaginas() {
+    return numeroDePaginas;
   }
 }
