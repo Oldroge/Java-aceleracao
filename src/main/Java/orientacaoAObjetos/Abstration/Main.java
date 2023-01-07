bdd4d436b9e1241747d812743634bbd80e3e2f44
oldroge <rogeriomrjr@gmail.com>
2023-01-06 22:45:30 -0300

    study: instanciando e executando métodos

--- src/main/Java/orientacaoAObjetos/Abstration/Main.java
+++ src/main/Java/orientacaoAObjetos/Abstration/Main.java
@@ -3,5 +3,7 @@ package orientacaoAObjetos.Abstration;
 public class Main {
   public static void main(String[] args) {
     Livro livro = new Livro("asd");
+    int numeroDePaginas = livro.retornarNumeroDePaginas();
+    System.out.println(numeroDePaginas);
   }
 }
