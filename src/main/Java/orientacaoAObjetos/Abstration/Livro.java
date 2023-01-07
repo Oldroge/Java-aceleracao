a878b19262d737553751a040c2d27b81c6f6f1db
oldroge <rogeriomrjr@gmail.com>
2023-01-06 22:42:43 -0300

    study: entendendo as propriedades

--- src/main/Java/orientacaoAObjetos/Abstration/Livro.java
+++ src/main/Java/orientacaoAObjetos/Abstration/Livro.java
@@ -2,6 +2,9 @@ package orientacaoAObjetos.Abstration;
 
 public class Livro {
   public Livro(String message) {
+    String titulo;
+    int numeroDePaginas;
+    String isbn;
     System.out.println(message);
   }
 
