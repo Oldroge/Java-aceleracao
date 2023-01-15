11e9c3ea45d65e5454b229ee1cf1863d0798289f
oldroge <rogeriomrjr@gmail.com>
2023-01-15 09:56:19 -0300

    study: exemplos de override (sobrescrever) métodos

---
+++ src/main/Java/orientacaoAObjetos/polimorfismo/Carro.java
@@ -0,0 +1,10 @@
+package orientacaoAObjetos.polimorfismo;
+
+public class Carro extends Veiculo {
+  private String modelo;
+
+  public void acelerar() {
+    System.out.println("Método acelerar da classe Carro.");
+  }
+
+}
