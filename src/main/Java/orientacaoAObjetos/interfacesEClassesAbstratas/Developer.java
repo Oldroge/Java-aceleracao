940c492c614ce69e109262c3be5aaf136357ce57
oldroge <rogeriomrjr@gmail.com>
2023-01-15 19:34:57 -0300

    study: implementação de interfaces em superclasses e subclasses

--- src/main/Java/orientacaoAObjetos/interfacesEClassesAbstratas/Developer.java
+++ src/main/Java/orientacaoAObjetos/interfacesEClassesAbstratas/Developer.java
@@ -1,11 +1,9 @@
 package orientacaoAObjetos.interfacesEClassesAbstratas;
 
-public abstract class Developer {
+public abstract class Developer implements SkillFull {
   public String language;
   public int yearsOfExperience = 2;
 
   public abstract double wage();
-
-  public abstract String mainSkill();
 
 }
