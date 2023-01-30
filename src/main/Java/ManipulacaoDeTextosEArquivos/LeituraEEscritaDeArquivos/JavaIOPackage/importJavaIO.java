5e84b505f695f5d23248989aedf644cbf4346c53
oldroge <rogeriomrjr@gmail.com>
2023-01-29 21:50:09 -0300

    study: exemplos de métodos a se utilizar com a classe File

--- src/main/Java/ManipulacaoDeTextosEArquivos/LeituraEEscritaDeArquivos/JavaIOPackage/importJavaIO.java
+++ src/main/Java/ManipulacaoDeTextosEArquivos/LeituraEEscritaDeArquivos/JavaIOPackage/importJavaIO.java
@@ -4,5 +4,11 @@ import java.io.FileReader; // Importação da classe auxilar para leitura de arq
 import java.io.FileWriter; // Importação da classe auxilar para escrita de arquivo
 
 public class importJavaIO {
-
+  File meuFile = new File("/path/to/some/file.txt");
+  
+  boolean arquivoExiste = meuFile.exists(); // VERIFICA SE O ARQUIVO OU DIRETÓRIO EXISTE
+  boolean podeExecutar = meuFile.canExecute(); // VERIFICA SE O PROGRAMA PODE EXECUTAR O ARQUIVO OU DIRETÓRIO
+  boolean podeLer = meuFile.canRead(); //  VERIFICA SE O PROGRAMA PODE LER O ARQUIVO OU DIRETÓRIO
+  boolean podeEscrever = meuFile.canWrite(); //  VERIFICA SE O PROGRAMA PODE ESCREVER O ARQUIVO OU DIRETÓRIO
+  boolean deletou = meuFile.delete(); // DELETA O ARQUIVO
 }
