f4ed51567583365d228e89b0a4eb667f42a40d5c
oldroge <rogeriomrjr@gmail.com>
2023-01-29 21:48:33 -0300

    study: importação da classe File

---
+++ src/main/Java/ManipulacaoDeTextosEArquivos/LeituraEEscritaDeArquivos/JavaIOPackage/importJavaIO.java
@@ -0,0 +1,8 @@
+package ManipulacaoDeTextosEArquivos.LeituraEEscritaDeArquivos.JavaIOPackage;
+import java.io.File; // Importação da classe que representa um arquivo.
+import java.io.FileReader; // Importação da classe auxilar para leitura de arquivo.
+import java.io.FileWriter; // Importação da classe auxilar para escrita de arquivo
+
+public class importJavaIO {
+
+}
