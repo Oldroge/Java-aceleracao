419c03611572ae14a0288d40d3bca48da00158df
oldroge <rogeriomrjr@gmail.com>
2023-02-06 13:41:49 -0300

    study: utilizando a classe Optional com o método ofNullable

--- src/main/Java/tratamentoDeErros/tryCatch/OptionalExample.java
+++ src/main/Java/tratamentoDeErros/tryCatch/OptionalExample.java
@@ -5,8 +5,20 @@ import java.util.Optional;
 public class OptionalExample {
 
   public static void main(String[] args) {
+    // Tratando erros de objetos nulos com o Optional vazio
+    // Desta forma conseguimos identificar se o objeto existe.
     Optional<Pessoa> optional = Optional.empty();
     if (optional.isPresent()) {
+      System.out.println("Pessoa existe");
+    } else {
+      System.out.println("Pessoa não existe");
+    }
+
+    // Tratando objetos nullos com ofNullable, passando uma instancia
+    // desta maneira consegue identificar se de fato existe algo dentro do objeto instanciado.
+    Pessoa pessoa2 = new Pessoa("Bob");
+    Optional<Pessoa> optional2 = Optional.ofNullable(pessoa2);
+    if (optional2.isPresent()) {
       System.out.println("Pessoa existe");
     } else {
       System.out.println("Pessoa não existe");
