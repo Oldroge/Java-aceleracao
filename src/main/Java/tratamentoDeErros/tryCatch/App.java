781ad43b42154e1d8f73e9c0f4dab3c99e4b55ed
oldroge <rogeriomrjr@gmail.com>
2023-02-06 13:04:04 -0300

    study: exemplo de método checked no qual compilador obriga a lançar exception

--- src/main/Java/tratamentoDeErros/tryCatch/App.java
+++ src/main/Java/tratamentoDeErros/tryCatch/App.java
@@ -1,5 +1,11 @@
 package tratamentoDeErros.tryCatch;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+
 public class App {
 
   public static void main(String[] args) {
@@ -12,6 +18,18 @@ public class App {
     // Exception in thread "main" java.lang.ArithmeticException: / by zero
     // at tratamentoDeErros.tryCatch.App.main(App.java:6)
 
+  }
+
+  /**
+   * Esse é um exemplo de método 'checked' no qual o compilador nos obriga a lançar uma excessão
+   * para tratamento de erro.
+   * 
+   * @throws FileNotFoundException
+   * @throws IOException
+   */
+  public static ObjectOutputStream leArquivo() throws FileNotFoundException, IOException {
+    File file = new File("arquivo_nao_existe.txt");
+    return new ObjectOutputStream(new FileOutputStream(file));
   }
 
 }
