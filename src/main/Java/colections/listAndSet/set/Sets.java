edad764188825c73008f504f5202b17735d770cb
oldroge <rogeriomrjr@gmail.com>
2023-02-09 19:56:18 -0300

    study: manipulando o set com o método .of()

--- src/main/Java/colections/listAndSet/set/Sets.java
+++ src/main/Java/colections/listAndSet/set/Sets.java
@@ -1,5 +1,6 @@
 package colections.listAndSet.set;
 
+import java.util.Set;
 import java.util.TreeSet;
 
 public class Sets {
@@ -14,6 +15,16 @@ public class Sets {
     numeros.add(1);
     numeros.add(10);
     System.out.println(numeros);
+
+    System.out.println("====================================");
+    System.out.println("Manipulando o Set:");
+    /**
+     * Manipular o Set tem semalhaças ao manipular um list, inclusivo nos seus métodos, por exemplo:
+     */
+    var estudantes = Set.of("João", "Maria", "Pedro", "Rosa");
+    System.out.println("usando o Set.of()");
+    System.out.println(estudantes);
+
 
   }
 
