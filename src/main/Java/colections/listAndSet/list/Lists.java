5979ec14cfa795bbc9ab29ea1798df17fd5da55b
oldroge <rogeriomrjr@gmail.com>
2023-02-09 15:15:26 -0300

    study: collection list - utilização do método sort e suas peculiaridades

--- src/main/Java/colections/listAndSet/list/Lists.java
+++ src/main/Java/colections/listAndSet/list/Lists.java
@@ -1,6 +1,7 @@
 package colections.listAndSet.list;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Lists {
 
@@ -20,6 +21,19 @@ public class Lists {
      * Reparando neste output, foi visto que seguiu uma ordem de entrada dos valores, ou seja, a
      * lista será lida na ordem de inserção.
      */
+
+    /**
+     * É possível ordenar os ítens dentro de uma lista de maneira comparativa, do menor para o menor
+     * e vice-versa
+     */
+    Collections.sort(numeros); // Collections aqui não é o mesmo que a interface (Collection)
+    System.out.println(numeros); // [1, 2, 7, 9]
+    /**
+     * Após order dessa maneira se acrescentar mais um ítem ele não entra na ordem de comparação que
+     * sort atrubuí, por exemplo:
+     */
+    numeros.add(3); // Após o sorte estamos acrescentando mais um valor a lista
+    System.out.println(numeros); // [1, 2, 7, 9, 3]
   }
 
 }
