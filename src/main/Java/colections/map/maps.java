4fbf52153e538a1e949b98f8ddea5ad3d57d84d4
oldroge <rogeriomrjr@gmail.com>
2023-02-10 13:45:21 -0300

    study: HashMap, como adicionar e alterar chaves e valores usando o método .put(key, value)

---
+++ src/main/Java/colections/map/maps.java
@@ -0,0 +1,23 @@
+package colections.map;
+
+import java.util.HashMap;
+
+class Main {
+  public static void main(String[] args) {
+    /**
+     * Usando o HashMap para criar dicionários de chave e valor
+     */
+    var estudantes = new HashMap<String, String>();
+    /**
+     * Com o método .put(key, value), adiciona novas chaves e valores
+     * no dicionário 'estudantes'.
+     */
+    estudantes.put("x038702", "João");
+    estudantes.put("x784244", "Maria");
+    estudantes.put("x105852", "Joana");
+    String valorAnterior = estudantes.put("x038702", "José"); // Mesma matricula usada no mapeamento com João
+    System.out.println("Valor anterior de x038702: " + valorAnterior); // valorAnterior imprime o valor substituído
+    System.out.println("Estudantes: " + estudantes); // {x038702=José, x105852=Joana, x784244=Maria}
+
+  }
+}
