f8b31409524c82741349feb94b389995359a232f
oldroge <rogeriomrjr@gmail.com>
2023-02-11 23:56:55 -0300

    study: criando classe estudante para testar streams

---
+++ src/main/Java/colections/streams/Estudante.java
@@ -0,0 +1,28 @@
+package colections.streams;
+
+public class Estudante {
+  private String matricula;
+
+  private String nome;
+
+  private String curso;
+
+  public Estudante(String matricula, String nome, String curso) {
+    this.matricula = matricula;
+    this.nome = nome;
+    this.curso = curso;
+  }
+
+  public String getMatricula() {
+    return matricula;
+  }
+
+  public String getNome() {
+    return nome;
+  }
+
+  public String getCurso() {
+    return curso;
+  }
+
+}
