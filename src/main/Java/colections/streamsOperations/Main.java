fea9f4798372a91aedf3b30dd2b2ba12d345cf93
oldroge <rogeriomrjr@gmail.com>
2023-02-12 20:39:46 -0300

    study: debugando código com o método peek()

--- src/main/Java/colections/streamsOperations/Main.java
+++ src/main/Java/colections/streamsOperations/Main.java
@@ -3,6 +3,7 @@ package colections.streamsOperations;
 import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Main {
 
@@ -13,6 +14,13 @@ public class Main {
 
     String firstName = nomes.stream().filter(a -> a == "Maria").collect(Collectors.toList()).get(0);
     System.out.println(firstName);
+
+
+    Collection entrada2 = Stream.of("um", "dois", "três", "quatro").filter(e -> e.length() > 3)
+        .peek(e -> System.out.println("Valor filtrado: " + e)).map(String::toUpperCase)
+        .peek(e -> System.out.println("Valor mapeado: " + e)).collect(Collectors.toList());
+
+    System.out.println(entrada2);
   }
 
 }
