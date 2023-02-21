687ff7ef2363ffbaee2f72c7f79ae387e87dae5e
oldroge <rogeriomrjr@gmail.com>
2023-02-21 13:20:56 -0300

    study: entendendo a classe abstrata Clock que trás o método systemDefaultZone

---
+++ src/main/Java/datas/datasComHorario/Main.java
@@ -0,0 +1,13 @@
+package datas.datasComHorario;
+
+import java.time.Clock;
+
+public class Main {
+
+  public static void main(String[] args) {
+    Clock relogio = Clock.systemDefaultZone();
+    System.out.println(relogio);
+
+  }
+
+}
