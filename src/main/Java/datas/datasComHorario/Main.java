f4b238f5032440ae5a8fb235bbc329afd0bbf944
oldroge <rogeriomrjr@gmail.com>
2023-02-21 13:26:46 -0300

    docs: comentando sobre diferença entre classes Clock e LocalDateTime

--- src/main/Java/datas/datasComHorario/Main.java
+++ src/main/Java/datas/datasComHorario/Main.java
@@ -1,14 +1,27 @@
 package datas.datasComHorario;
 
 import java.time.Clock;
+import java.time.LocalDateTime;
 
 public class Main {
 
   public static void main(String[] args) {
     Clock relogio = Clock.systemDefaultZone();
+
+    System.out.println("Usando Classe abstrata Clock: ");
     System.out.println(relogio);
 
     System.out.println(relogio.instant());
+
+    System.out.println("Usando LocalDateTime: ");
+
+    LocalDateTime now = LocalDateTime.now();
+    System.out.println(now);
+
+    /**
+     * A diferença entre LocalDateTime.now() e Clock.instant() é que o instante pega base no fuso
+     * originário UTC, enquanto o LocalDateTime.now() pega com base na região onde eu configurei.
+     */
 
   }
 
